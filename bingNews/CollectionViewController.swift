4775bca9804f0808844ffee9468a030a39665f75
alexfu <afu@dynosense.com>
2017-03-20 23:05:33 -0700

    1

--- bingNews/CollectionViewController.swift
+++ bingNews/CollectionViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 private let reuseIdentifier = "Cell"
 
@@ -20,13 +21,12 @@ class CollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: view.bounds.width, height: 150)
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         layout.minimumInteritemSpacing = 0
         layout.minimumLineSpacing = 0
-   
-        collectionView?.collectionViewLayout = layout
+        collectionView?.prefetchDataSource = self
+    
 
         
         NewsAPI.share.sendNewsRequest { articles in
@@ -60,10 +60,38 @@ extension CollectionViewController {
 }
 
 // MARK: UICollectionViewDelegate
-extension CollectionViewController {
+extension CollectionViewController:UICollectionViewDataSourcePrefetching {
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         //       let character = articles[indexPath.item]
         //       performSegue(withIdentifier: "MasterToDetail", sender: character)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        print("prefetch")
+        for indexPath in indexPaths {
+            print(indexPath.item)
+            let article = articles[indexPath.row]
+            let start = CFAbsoluteTimeGetCurrent()
+            
+            article.task = ImageDownloader.default.downloadImage(with: article.thumbnail, options: [], progressBlock: nil) {
+                (image, error, url, data) in
+                let end = CFAbsoluteTimeGetCurrent()
+                print(end - start)
+                article.image = image
+                //(cell as! CollectionViewCell).cellImageView.image = image
+            }
+        }
+    }
+    
+    // indexPaths that previously were considered as candidates for pre-fetching, but were not actually used; may be a subset of the previous call to -collectionView:prefetchItemsAtIndexPaths:
+    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+        
+        for indexPath in indexPaths {
+            let article = articles[indexPath.row]
+            article.task?.cancel()
+        }
+    }
+    
+
 }
 
